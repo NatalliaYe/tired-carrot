cf9e0f9667af8e67c62ae2a1b505f66bc1c08d4b
Natalya <natalyhubchuk@gmail.com>
2025-08-30 11:00:38 +0300

    Second Try. Added Homework part1 -Name, lastName, profession. Homework part2 - Declared and initialized variables. Homework part3 - imported scanner, printed out result of all studied string methods

--- src/MainHomework1.java
+++ src/MainHomework1.java
@@ -2,12 +2,10 @@
 
 public class MainHomework1 {
     public static void main(String[] args) {
-        // Создаем переменные с данными
         String firstName = "Natalya";
         String lastName = "Yeliseikina";
         String profession = "QA Engineer";
 
-        // Выводим информацию
         System.out.println("Имя: " + firstName);
         System.out.println("Фамилия: " + lastName);
         System.out.println("Профессия: " + profession);
